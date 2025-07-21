a6175a7143f2348c5566cec7fe3afa0f3dda8bdd
Natalia Goncharenko <muratovan0921@gmail.com>
2025-07-21 23:36:36 +0300

    add parser for json and yaml

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,46 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class Parser {
+    public static Map<String, Object> parse(String filepath) throws Exception {
+        InputStream is = Parser.class.getClassLoader().getResourceAsStream(filepath);
+        String file;
+        if (is == null) {
+            Path path = Paths.get(filepath).toAbsolutePath().normalize();
+            if (!Files.exists(path)) {
+                throw new Exception("File '" + path + "' does not exist");
+            }
+            file = Files.readString(path);
+        } else {
+            file = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        var a = filepath.lastIndexOf('.');
+        var format = filepath.substring(a + 1);
+        if (format.equals("json")) {
+            return jsonParser(file);
+        } else if (format.equals("yaml") || format.equals("yml")) {
+            return yamlParser(file);
+        }
+        throw new Exception("Unknown format");
+    }
+
+    public static Map<String, Object> jsonParser(String jsonFile) throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonFile, new TypeReference<>() { });
+    }
+
+    public static Map<String, Object> yamlParser(String yamlFile) throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        return objectMapper.readValue(yamlFile, new TypeReference<>() { });
+    }
+}
