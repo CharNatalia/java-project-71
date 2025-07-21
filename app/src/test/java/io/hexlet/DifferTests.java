e35f5e66cb6e811fb44e0a0a059b7f03aa889f5b
Natalia Goncharenko <muratovan0921@gmail.com>
2025-07-21 23:37:36 +0300

    add tests for Differ

--- app/src/test/java/io/hexlet/DifferTests.java
+++ app/src/test/java/io/hexlet/DifferTests.java
@@ -1,29 +1,57 @@
 package io.hexlet;
 
 import hexlet.code.Differ;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTests {
     @Test
-    public void testDiffer() throws Exception {
+    public void testJson() throws Exception {
         String expected = """
                 - follow: false
                   host: hexlet.io
                 - proxy: 123.234.53.22
                 - timeout: 50
                 + timeout: 20
-                + verbose: true
-                """;
-        var previousFilePath = Paths.get("./src/test/resources/file1.json").toAbsolutePath().normalize();
-        var currentFilePath = Paths.get("./src/test/resources/file2.json").toAbsolutePath().normalize();
-        var previousFile = Files.readString(previousFilePath).trim();
-        var currentFile = Files.readString(currentFilePath).trim();
+                + verbose: true""";
+        Map<String, Object> previousFile = new HashMap<>();
+        previousFile.put("host", "hexlet.io");
+        previousFile.put("timeout", 50);
+        previousFile.put("proxy", "123.234.53.22");
+        previousFile.put("follow", false);
+
+        Map<String, Object> currentFile = new HashMap<>();
+        currentFile.put("timeout", 20);
+        currentFile.put("verbose", true);
+        currentFile.put("host", "hexlet.io");
+
         String actual = Differ.generate(previousFile, currentFile);
-        assertEquals(expected.replace("\n", ""), actual.replace("\n", ""));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("test with null object")
+    public void testJson2() {
+        String expected = """
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null""";
+        Map<String, Object> previousFile = new HashMap<>();
+        previousFile.put("id", 45);
+        previousFile.put("default", null);
+
+        Map<String, Object> currentFile = new HashMap<>();
+        currentFile.put("id", null);
+        currentFile.put("default", List.of("value1", "value2"));
+
+        String actual = Differ.generate(previousFile, currentFile);
+        assertEquals(expected, actual);
     }
 }
